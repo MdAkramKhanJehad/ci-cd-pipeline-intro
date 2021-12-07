4b353d7e0502c52f8ef78e0d1c68a8e1dc39cbf1
Md. Akram Khan <bsse1007@iit.du.ac.bd>
2021-12-07 19:56:20 +0600

    initial push

---
+++ index.py
@@ -0,0 +1,39 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return {
+        'Message': 'Hello World!',
+    }
+
+@app.route('/multi/<a>/<b>')
+def multi_route(a, b):
+    return {
+        'Multi result': multi(a, b)
+    }
+
+
+def multi(a,b):
+    if type(a) == str and not a.isdigit():
+        return "Invalid Input"
+
+    if type(b) == str and not b.isdigit():
+        return "Invalid Input"
+
+    a = int(a)
+    b = int(b)
+    return a * b
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+
+
+
+
---
+++ test_index.py
@@ -0,0 +1,5 @@
+from index import multi
+
+def test_multi():
+    assert nulti(1,2) == 2
+    assert multi(1, "asdf") == "Invalid Input"
